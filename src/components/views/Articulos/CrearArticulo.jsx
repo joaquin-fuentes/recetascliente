3450ef47696ee49b568d060d7734fc73ca768cae
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2023-06-17 10:59:02 -0300

    base de datos creada

--- src/components/views/Articulos/CrearArticulo.jsx
+++ src/components/views/Articulos/CrearArticulo.jsx
@@ -91,7 +91,7 @@ const CrearArticulo = () => {
                 <Form.Group className="mb-3">
                     <Form.Label>Nombre de la receta*</Form.Label>
                     <Form.Control type="text" placeholder="Ej: Cafe" maxLength={30} {
-                        ...register('nombreProducto', {
+                        ...register('nombreArticulo', {
                             required: 'El campo es obligatorio',
                             minLength: {
                                 value: 2,
@@ -108,7 +108,7 @@ const CrearArticulo = () => {
                         })
                     } />
                     <Form.Text className="text-danger">
-                        {errors.nombreProducto?.message}
+                        {errors.nombreArticulo?.message}
                     </Form.Text>
                 </Form.Group>
                 <Form.Group className="mb-3">
